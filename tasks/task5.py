5c5f975e7fd0973777b7c0a57bb2f9f2f50fc25c
Safarov Arthur <ttttwobata@gmail.com>
2025-11-24 00:34:11 +0500

    Update task5.py

--- tasks/task5.py
+++ tasks/task5.py
@@ -2,7 +2,9 @@
 
 def solve():
 # Ниже пишите решение задачи
-    
+    chars = input().split()
+    for char in chars:
+    print(f"Код символа {char} равен {ord(char)}")
    
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
